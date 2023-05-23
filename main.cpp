57c96d2fff622c68861e0f0346c99c72b59ce435
侑汰 林 <220311@jc-21.jp>
2023-05-23 10:46:31 +0900

    ブラッシュアップ

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <Windows.h>
 
+//定数宣言
+const char* WIN_CLASS_NAME = "SampleGame";  //ウィンドウクラス名
+const char* WIN_GAME_NAME = "サンプルゲーム";
+const int WINDOW_WIDTH = 800;  //ウィンドウの幅
+const int WINDOW_HEIGHT = 600; //ウィンドウの高さ
+
 //プロトタイプ宣言(ウィンドウプロシージャ)
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
@@ -12,7 +18,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
 
 	wc.cbSize = sizeof(WNDCLASSEX);             //この構造体のサイズ
 	wc.hInstance = hInstance;                   //インスタンスハンドル
-	wc.lpszClassName = "SampleGame";            //ウィンドウクラス名
+	wc.lpszClassName = WIN_CLASS_NAME;            //ウィンドウクラス名
 	wc.lpfnWndProc = WndProc;                   //ウィンドウプロシージャ
 	wc.style = CS_VREDRAW | CS_HREDRAW;         //スタイル（デフォルト）
 	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); //アイコン
@@ -24,15 +30,22 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
 	wc.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH); //背景（白）
 	RegisterClassEx(&wc);  //クラスを登録
 
+	//ウィンドウサイズの計算
+	RECT winRect = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+	AdjustWindowRect(&winRect, WS_OVERLAPPEDWINDOW, FALSE);//FALSE メニューの有無　ある場合true
+	int winW = winRect.right - winRect.left;     //ウィンドウ幅
+	int winH = winRect.bottom - winRect.top;     //ウィンドウ高さ
+
+
    //ウィンドウを作成
 	HWND hWnd = CreateWindow(
-		"SampleGame",         //ウィンドウクラス名
-		"サンプルゲーム",     //タイトルバーに表示する内容
+		WIN_CLASS_NAME,         //ウィンドウクラス名
+		WIN_GAME_NAME,     //タイトルバーに表示する内容
 		WS_OVERLAPPEDWINDOW, //スタイル（普通のウィンドウ）
 		CW_USEDEFAULT,       //表示位置左（おまかせ）
 		CW_USEDEFAULT,       //表示位置上（おまかせ）
-		800,                 //ウィンドウ幅
-		600,                 //ウィンドウ高さ
+		winW,                 //ウィンドウ幅
+		winH,                 //ウィンドウ高さ
 		NULL,                //親ウインドウ（なし）
 		NULL,                //メニュー（なし）
 		hInstance,           //インスタンス
