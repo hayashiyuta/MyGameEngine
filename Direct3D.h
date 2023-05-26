73ea8fc5e6b44323694f4b4559161814c6e01aeb
侑汰 林 <220311@jc-21.jp>
2023-05-26 11:34:07 +0900

    シェーダー:頂点インプットレイアウトまで

--- Direct3D.h
+++ Direct3D.h
@@ -3,30 +3,25 @@
 #include <d3d11.h>
 //リンカ Direct3Dの各機能が使えるようになる
 #pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
 namespace Direct3D
 
 {
 
 	//初期化
-
 	void Initialize(int winW, int winH, HWND hWnd);
 
-
+	//シェーダー準備
+	void InitShader();
 
 	//描画開始
-
 	void BeginDraw();
 
-
-
 	//描画終了
-
 	void EndDraw();
 
-
-
 	//解放
-
 	void Release();
 
 };
