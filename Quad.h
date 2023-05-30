55053ec0ddd78e78d7b9a7cf3b86da8f70623705
侑汰 林 <220311@jc-21.jp>
2023-05-30 11:33:36 +0900

    ポリゴン表示　コンスタントバッファ作成まで

---
+++ Quad.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <DirectXMath.h>
+#include "Direct3D.h"
+
+using namespace DirectX;
+
+//コンスタントバッファー
+struct CONSTANT_BUFFER
+{
+	XMMATRIX	matWVP;
+};
+
+
+class Quad
+{
+	ID3D11Buffer* pVertexBuffer_;	//頂点バッファのアドレスを入れる
+	//メンバ変数を作ったらコンストラクタで初期化
+	ID3D11Buffer* pIndexBuffer_;//インデックスバッファ
+	ID3D11Buffer* pConstantBuffer_;	//コンスタントバッファ
+
+
+public:
+	Quad();
+	~Quad();
+	void Initialize();
+	void Draw();
+	void Release();
+
+};
+
